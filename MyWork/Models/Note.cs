7c3d316e9d4c3b2d662216a86f77eceaf7385cee
BlazeRFeeD <baser.minecrafter@gmail.com>
2020-11-29 04:25:38 +0300

    Добавил класс записи

---
+++ MyWork/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWork.Models
+{
+    public class Note
+    {
+        public string OrganizationName { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public int Hours { get; set; }
+    }
+}
