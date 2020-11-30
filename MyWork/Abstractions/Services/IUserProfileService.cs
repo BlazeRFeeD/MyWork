b90e3d6537057cdd37705091e0355e7aff038d47
BlazeRFeeD <baser.minecrafter@gmail.com>
2020-12-01 02:02:18 +0300

    Добавил интерфейс IUserProfileService и его дефолтную реализацию

---
+++ MyWork/Abstractions/Services/IUserProfileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyWork.Models;
+
+namespace MyWork.Abstractions.Services
+{
+    public interface IUserProfileService
+    {
+        UserProfile ReadFromFile(string path);
+
+        void Save(UserProfile userProfile, string path);
+    }
+}
---
+++ MyWork/Services/UserProfileService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using MyWork.Abstractions.Services;
+using MyWork.Models;
+
+namespace MyWork.Services
+{
+    public class UserProfileService : IUserProfileService
+    {
+        public UserProfile ReadFromFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Файл с профилем по заданному пути не найден");
+
+            var text = File.ReadAllText(path);
+            var profile = JsonSerializer.Deserialize<UserProfile>(text);
+
+            return profile;
+        }
+
+        public void Save(UserProfile userProfile, string path)
+        {
+            var json = JsonSerializer.Serialize(userProfile);
+
+            var result = File.CreateText(path);
+            result.Close();
+            result.Dispose();
+        }
+    }
+}
