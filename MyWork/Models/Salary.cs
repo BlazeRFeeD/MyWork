3a9c30ed5ea775a375c70e40a735552d12fb0c3a
BlazeRFeeD <baser.minecrafter@gmail.com>
2020-12-01 02:02:38 +0300

    Добавил класс зарплаты и профиля пользователя

---
+++ MyWork/Models/Salary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWork.Models
+{
+    public class Salary
+    {
+        public int SalaryRate { get; set; }
+
+        public bool IsHalfTime { get; set; }
+
+        public int HourCost { get; set; }
+    }
+}
---
+++ MyWork/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpDX.Direct2D1;
+
+namespace MyWork.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public Salary Salary { get; set; }
+    }
+}
