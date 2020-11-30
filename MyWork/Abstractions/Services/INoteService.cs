921f12405f4e151915a340634b6e639e8c5dcb1d
BlazeRFeeD <baser.minecrafter@gmail.com>
2020-12-01 02:13:08 +0300

    Добавил интерфейс INoteService и его дефолтную реализацию

---
+++ MyWork/Abstractions/Services/INoteService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyWork.Models;
+
+namespace MyWork.Abstractions.Services
+{
+    public interface INoteService
+    {
+        List<Note> ReadFromFile(string path);
+
+        void Save(List<Note> notes, string path);
+
+    }
+}
---
+++ MyWork/Services/NoteService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using MyWork.Abstractions.Services;
+using MyWork.Models;
+
+namespace MyWork.Services
+{
+    public class NoteService : INoteService
+    {
+        public List<Note> ReadFromFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Файл с записями по заданному пути не найден");
+
+            var text = File.ReadAllText(path);
+            var notes = JsonSerializer.Deserialize<List<Note>>(text);
+
+            return notes;
+        }
+
+        public void Save(List<Note> notes, string path)
+        {
+            var json = JsonSerializer.Serialize(notes);
+
+            File.WriteAllText(path, json);
+        }
+    }
+}
