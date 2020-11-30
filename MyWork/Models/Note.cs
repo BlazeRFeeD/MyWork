b20613debd54043d23698536b955d2ca48216eb9
BlazeRFeeD <baser.minecrafter@gmail.com>
2020-12-01 02:03:09 +0300

    Добавил комментарии и флаг IsPayable

--- MyWork/Models/Note.cs
+++ MyWork/Models/Note.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace MyWork.Models
 {
     public class Note
     {
+        /// <summary>
+        /// Название организации
+        /// </summary>
         public string OrganizationName { get; set; }
 
+        /// <summary>
+        /// Наименование работ
+        /// </summary>
         public string ServiceName { get; set; }
 
+        /// <summary>
+        /// Количество потраченных часов
+        /// </summary>
         public int Hours { get; set; }
+
+        /// <summary>
+        /// Дата исполнения
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Оплачевается ли работа
+        /// </summary>
+        public bool IsPayable { get; set; }
     }
 }
